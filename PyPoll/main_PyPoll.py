6a9e108e1158d027848f84a32a82109d59a5b25d
skylar17 <64751046+skylar17@users.noreply.github.com>
2020-06-02 22:29:00 -0400

    updated code

--- PyPoll/main_PyPoll.py
+++ PyPoll/main_PyPoll.py
@@ -2,7 +2,7 @@ import os
 import csv
 
 # Set a CSV file path
-csvpath = os.path.join("Resources_PyPoll", "election_data.csv")
+csvpath = os.path.join("PyPoll", "Resources_PyPoll", "election_data.csv")
 
 # Reading in the CSV file 
 with open(csvpath) as csvfile:
@@ -10,17 +10,36 @@ with open(csvpath) as csvfile:
     next(csvreader, None)   # Excludes headers
 
     # Declaration of lists and variables
-    all_votes = []
+    votes_casted = []
     unique_candidates = []
+    candidate_count = []
+    percent = []
 
     for z, row in enumerate(csvreader):
-        all_votes.append(row[2])
+        votes_casted.append(row[2])
 
+    
+        # A complete list of candidates who received votes (uniques from Candidate column)
+        if row[2] not in unique_candidates:
+            
+            unique_candidates.append(row[2])
+            #x = len(unique_candidates)
+            #print(f"{x}")
 
 # The total number of votes cast
-total_votes = len(all_votes)
+total_votes = len(votes_casted)
 
-# A complete list of candidates who received votes (uniques from Candidate column)
-if row[2] not in unique_candidates:
-            unique_candidates.append(row[2])
+
+    # for candidate in unique_candidates:
+    #         candidate_count.append(votes_casted.count(candidate))
+    #         percent.append(round(votes_casted.count(candidate)/total_votes*100,3))
+
+
+print("Election Results")
+print("-----------------------------------")
+print(f"Total Votes: {total_votes}")
+print("-----------------------------------")
+
+
+
 
