99371aa10620d1c7da4cfae0c8b929ed5130dd32
skylar17 <64751046+skylar17@users.noreply.github.com>
2020-06-02 22:59:56 -0400

    almost final pypoll

--- PyPoll/main_PyPoll.py
+++ PyPoll/main_PyPoll.py
@@ -18,27 +18,44 @@ with open(csvpath) as csvfile:
     for z, row in enumerate(csvreader):
         votes_casted.append(row[2])
 
-    
+
+        # The total number of votes cast
+        total_votes = len(votes_casted)
+
+
         # A complete list of candidates who received votes (uniques from Candidate column)
         if row[2] not in unique_candidates:
             
             unique_candidates.append(row[2])
-            #x = len(unique_candidates)
-            #print(f"{x}")
+            
 
-# The total number of votes cast
-total_votes = len(votes_casted)
+    # The total number of votes and The percentage of votes each candidate won
+    for candidate in unique_candidates:
+            candidate_count.append(votes_casted.count(candidate))
+            percent.append(round(votes_casted.count(candidate)/total_votes*100, 3))
 
 
-    # for candidate in unique_candidates:
-    #         candidate_count.append(votes_casted.count(candidate))
-    #         percent.append(round(votes_casted.count(candidate)/total_votes*100,3))
+# The winner of the election based on popular vote
+max_votes= max(candidate_count)  
+indx_winner = candidate_count.index(max_votes)  
+winner = unique_candidates[indx_winner]
+            
 
 
 print("Election Results")
 print("-----------------------------------")
 print(f"Total Votes: {total_votes}")
 print("-----------------------------------")
+
+for i in range(len(unique_candidates)):
+    print(f"{unique_candidates[i]}: {percent[i]}% ({candidate_count[i]})")
+
+print("-----------------------------------")
+print(f"Winner: {winner}")
+print("-----------------------------------")
+
+
+#------------------------------------------------------------------------------------------
 
 
 
